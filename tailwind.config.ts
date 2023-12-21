590be60318956d3ed0ec0d9bca93554200d3c68e
Viktor Ivanov <vitochek337@gmail.com>
2023-12-21 21:23:48 +0200

    Initial page setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,18 +2,25 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      colors: {
+        "snd-gray": {
+          100: "#F5F5DC",
+          200: "#A9A9A9",
+          300: "#474747",
+          500: "#333333",
+          800: "#1E1E1E",
+          900: "#0B0B0B"
+        },
+        "snd-red": {
+          700: "#800020"
+        }
+      }
+    }
   },
   plugins: [],
 }
